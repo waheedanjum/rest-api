1c3d1186ef00241c304b35e4477fd4e8693f1fca
waheedanjum <mwaheedanjum@outlook.com>
2020-09-28 00:32:55 +0100

    unit tests for data service started

--- rest-api/src/app/services/data.service.spec.ts
+++ rest-api/src/app/services/data.service.spec.ts
@@ -1,54 +1,62 @@
-import { getTestBed, TestBed } from "@angular/core/testing";
-import {
-  HttpClientTestingModule,
+import { TestBed } from "@angular/core/testing";
+import {  HttpClientTestingModule,
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { Server } from "../models/server";
 
 import { DataService } from "./data.service";
-import { doesNotThrow } from 'assert';
 
-export const servers: Server[] = [
-  {
-    uuid: "cb1cb873-0a46-41d6-affc-a1ed41955fbf",
-    mem: "1 GB",
-    state: "stopped",
-    cpu: "7 GHz",
-    name: "My Server 1",
-  },
-];
+import { RouterTestingModule } from "@angular/router/testing";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-describe("DataService", async() => {
-  let injector: TestBed;
+describe("DataService", () => {
   let dataService: DataService;
   let httpMock: HttpTestingController;
+ 
+  // Test Server Data
+  const serverData: Server[] = [
+    {
+      uuid: "cb1cb873-0a46-41d6-affc-a1ed41955fbf",
+      mem: "1 GB",
+      state: "stopped",
+      cpu: "7 GHz",
+      name: "My Server 1",
+    },
+    {
+      uuid: "01e3021c-cd5c-4d3f-b39a-6b2dd3f706ba",
+      mem: "2 GB",
+      state: "running",
+      cpu: "8 GHz",
+      name: "My Server 2",
+    },
+    {
+      uuid: "1d76a4b0-6887-43cc-97b3-8229c05b1fa1",
+      mem: "4 GB",
+      state: "stopped",
+      cpu: "9 GHz",
+      name: "My Server 3",
+    },
+  ];
 
-  beforeEach(() =>
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])],
       providers: [DataService],
-      schemas: [],
-    })
-  );
-
-  injector = getTestBed();
-  dataService = TestBed.get(DataService);
-  httpMock = injector.get(HttpTestingController);
-
-  afterEach(() => {
-    httpMock.verify();
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    });
   });
 
   it("should be created", async () => {
     const service: DataService = TestBed.get(DataService);
     expect(service).toBeTruthy();
-    
   });
 
-  it('should return Observable<Server> List', async()=>{
-    dataService.getServers().subscribe(s => {
-     console.log(s);
+  it("should GET List of Servers", () => {
+    dataService.getServers().subscribe((servers) => {
+      expect(servers[0]).toEqual(serverData[0]);
     });
+    const request = httpMock.expectOne("app/list-server-details");
+      request.flush([serverData[0]]);
+      httpMock.verify();
   });
 });
--- rest-api/src/app/services/data.service.ts
+++ rest-api/src/app/services/data.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Observable, of} from "rxjs";
 import { Server } from "../models/server";
 import { catchError} from "rxjs/operators";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: "root",
