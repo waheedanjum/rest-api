4191a0f6e29a46072ed7770cd8e054fd9101c609
waheedanjum <mwaheedanjum@outlook.com>
2020-09-27 17:55:46 +0100

    commit

--- rest-api/src/app/services/data.service.spec.ts
+++ rest-api/src/app/services/data.service.spec.ts
@@ -1,20 +1,54 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { getTestBed, TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { Server } from "../models/server";
 
-import { DataService } from './data.service';
+import { DataService } from "./data.service";
+import { doesNotThrow } from 'assert';
 
-describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    declarations: [
-      
-    ],
-    providers: [],
-    schemas: []
-  }));
+export const servers: Server[] = [
+  {
+    uuid: "cb1cb873-0a46-41d6-affc-a1ed41955fbf",
+    mem: "1 GB",
+    state: "stopped",
+    cpu: "7 GHz",
+    name: "My Server 1",
+  },
+];
 
-  it('should be created', async() => {
+describe("DataService", async() => {
+  let injector: TestBed;
+  let dataService: DataService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [],
+      providers: [DataService],
+      schemas: [],
+    })
+  );
+
+  injector = getTestBed();
+  dataService = TestBed.get(DataService);
+  httpMock = injector.get(HttpTestingController);
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it("should be created", async () => {
     const service: DataService = TestBed.get(DataService);
     expect(service).toBeTruthy();
+    
+  });
+
+  it('should return Observable<Server> List', async()=>{
+    dataService.getServers().subscribe(s => {
+     console.log(s);
+    });
   });
 });
